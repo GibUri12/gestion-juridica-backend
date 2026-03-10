d023f005fe09ac5da68c4ccd06525229c792aee6
Gibran-Diaz7 <gibran.diaz@redsoftgroup.mx>
2026-03-09 19:07:45 -0600

    primer commit

---
+++ src/main/java/com/despacho/gestion/GestionJuridicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.despacho.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionJuridicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionJuridicaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/despacho/gestion/GestionJuridicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.despacho.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionJuridicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
